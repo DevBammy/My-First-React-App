a3b73157873a6461c487618982dec147298aead9
bamidele <devbammy@gmail.com>
2025-10-16 12:03:26 +0100

    created the ui for the blog details page

--- src/pages/BlogDetails.jsx
+++ src/pages/BlogDetails.jsx
@@ -1,10 +1,32 @@
-import React from 'react';
 import { useParams } from 'react-router-dom';
+import { blogs as allBlogs } from '../assets/blogs';
+import { useState } from 'react';
+import moment from 'moment';
+import image from '../assets/hero.jpg';
+import styles from '../styles/blogDetails.module.css';
 
 const BlogDetails = () => {
   const { id } = useParams();
+  const [blogs, setBlogs] = useState(allBlogs);
 
-  return <div>BlogDetails {id}</div>;
+  const findBlog = blogs.find((b) => b.id == id);
+
+  return (
+    <div>
+      <div className={styles.image}>
+        <img src={image} alt="" />
+
+        <h1>{findBlog.title}</h1>
+      </div>
+
+      <div className={styles.blogBody}>
+        <p>{findBlog.body}</p>
+
+        <p>{moment(findBlog.date).fromNow()}</p>
+        <span>{findBlog.author}</span>
+      </div>
+    </div>
+  );
 };
 
 export default BlogDetails;
