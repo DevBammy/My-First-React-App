0ad9521018cf2c5aa90143a2db7c6600082276ab
D'vora <chukwudeborah17@gmail.com>
2025-10-16 13:19:22 +0100

    from the main

--- src/pages/BlogDetails.jsx
+++ src/pages/BlogDetails.jsx
@@ -1,29 +1,14 @@
 import { useParams } from 'react-router-dom';
-<<<<<<< HEAD
-import { blogs } from '../assets/blogs';
-
-=======
 import { blogs as allBlogs } from '../assets/blogs';
 import { useEffect, useState } from 'react';
 import moment from 'moment';
 import image from '../assets/hero.jpg';
 import styles from '../styles/blogDetails.module.css';
->>>>>>> 79f626daf337776eaa3ce8e2612d617bf9b98f8f
 
 const BlogDetails = () => {
   const { id } = useParams();
   const [blogs, setBlogs] = useState(allBlogs);
 
-<<<<<<< HEAD
-  const allBlogs = blogs
-
-  const singleBlog = allBlogs.find((blog)=>blog.id==id)
-
-  return <div>BlogDetails {singleBlog.name}</div>;
-  
-
-  
-=======
   const findBlog = blogs.find((b) => b.id == id);
 
   useEffect(() => {
@@ -46,7 +31,6 @@ const BlogDetails = () => {
       </div>
     </div>
   );
->>>>>>> 79f626daf337776eaa3ce8e2612d617bf9b98f8f
 };
 
 export default BlogDetails;
